f361701ec5f43aae14f30a6b973cd8671d6cbb0d
r351574nc3 <r351574nc3@gmail.com>
2015-03-15 16:58:10 -0700

    Setting up app to talk to firebase

--- src/main/java/com/github/quorawr/minecraft/App.java
+++ src/main/java/com/github/quorawr/minecraft/App.java
@@ -1,13 +1,16 @@
 package com.github.quorawr.minecraft;
 
+import com.firebase.client.
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public App() {
+        final Firebase fb = new Firebase("https://worldbuilder-fb.firebaseio.com");
+    }
+    
+    public static void main(String[] args) {        
     }
 }
