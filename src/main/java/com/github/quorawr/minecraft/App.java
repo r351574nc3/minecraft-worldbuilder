822c6d54c8a6efa19870278c8ff128a08100545a
` <Ashley@omega-behemoth.local>
2015-03-22 15:39:18 -0700

    When command is issued it shall broadcast the message 'hello'Added imports and Methods

--- src/main/java/com/github/quorawr/minecraft/App.java
+++ src/main/java/com/github/quorawr/minecraft/App.java
@@ -1,16 +1,46 @@
 package com.github.quorawr.minecraft;
 
-import com.firebase.client.
+import com.firebase.client.Firebase;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
 
 /**
  * Hello world!
  *
  */
-public class App {
+public class App extends JavaPlugin {
     public App() {
         final Firebase fb = new Firebase("https://worldbuilder-fb.firebaseio.com");
     }
+
+    public void onLoad() {
+
+    }
+
+    public void onDisable() {
+
+    }
     
-    public static void main(String[] args) {        
+    public void onEnable() {
+
+    }
+
+    public boolean onCommand(final CommandSender sender, 
+    						 final Command command,
+    						 final String label,
+    						 final String ... args) { 
+    	if (label.equalsIgnoreCase("hello")) {
+    		final String message = "Some message";
+    		getServer().broadcastMessage(message);
+    		return true;
+
+    	}
+
+    	return false;
+
+
     }
 }
